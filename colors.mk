e2b1bae462cb135d6d559e4b39f4d03d1458f08f
Francois Petit <fpetit@2G6.42angouleme.fr>
2025-09-16 17:12:01 +0200

    [feat] makefile with loading bar + format targets

---
+++ colors.mk
@@ -0,0 +1,23 @@
+NOC			= \e[0m
+BOLD		= \e[1m
+DIM			= \e[2m
+UNDERLINE	= \e[4m
+
+BLACK		= \e[1;30m
+RED			= \e[1m\e[38;5;196m
+GREEN		= \e[1m\e[38;5;76m
+YELLOW		= \e[1m\e[38;5;220m
+BLUE		= \e[1m\e[38;5;33m
+PURPLE		= \e[1;35m
+CYAN		= \e[1;36m
+WHITE		= \e[1;37m
+
+BLACK_BG 	= \e[0;40m
+RED_BG 		= \e[0;41m
+GREEN_BG 	= \e[0;42m
+YELLOW_BG 	= \e[0;43m
+BLUE_BG 	= \e[0;44m
+MAGENTA_BG 	= \e[0;45m
+CYAN_BG 	= \e[0;46m
+WHITE_BG 	= \e[0;47m
+RESET_BG	= \e[0m
